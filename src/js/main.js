ae30b8d41be4137c2a303c9191e97ae78d064b3e
Ula Krawczyk <ula.krawczyk7@gmail.com>
2020-04-20 22:16:07 +0200

    feat: adding modals with interior schemes to home section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,37 @@
 "use strict";
 
-// place your code below
+const homeButtons = document.querySelectorAll('.home__button');
+const modalOut = document.querySelector('.modal');
+const modalIn = document.querySelector('.modal__inner');
 
+function handleHomeButton(e) {
+  const button = e.currentTarget;
+  const card = button.closest('.home');
+  const imgSrc = card.querySelector('.home__img--js').src;
+  const desc = card.dataset.description;
 
-console.log(`Hello world!`);
+  modalIn.innerHTML = `
+    <img src="${imgSrc}" alt="chosen interior">
+    <p>${desc}</p>
+  `;
+  modalOut.classList.add('open');
+}
+
+homeButtons.forEach(button => {
+  button.addEventListener('click', handleHomeButton);
+});
+
+function closeModal() {
+  modalOut.classList.remove('open');
+}
+modalOut.addEventListener('click', function(e) {
+  const isOpen = !e.target.closest('.modal__inner');
+  if(isOpen) {
+    closeModal();
+  }
+});
+window.addEventListener('keydown', function(e) {
+  if(e.key === 'Escape') {
+    closeModal();
+  }
+});
